d2dcae53d8aee1885225e34b93357a0e87e62425
Wuchong123456 <68960535+Wuchong123456@users.noreply.github.com>
2020-09-19 23:36:25 +0800

    Create 通讯录 contact.h

---
+++ 通讯录 contact.h
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#define MAX 1000
+#define MAX_name 10
+#define MAX_sex 3
+#define MAX_tele 15
+#define MAX_addr 10
+struct peoinfo
+{
+	char name[MAX_name];
+	int age;
+	char sex[MAX_sex];
+	char tele[MAX_tele];
+	char addr[MAX_addr];
+};
+struct contact
+{
+	struct peoinfo data[MAX];
+	int size;
+};
+void initcontact(struct contact* ps);
+void addcontact(struct contact* pa);
+void showcontact(const struct contact* pa);
